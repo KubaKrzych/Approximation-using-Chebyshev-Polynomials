9583e36ee39f341615d39cc87d70c1e493ff2381
KubaKrzych <root@LAPTOP-UV8CD8O9.localdomain>
2021-12-28 10:15:41 +0100

    Modify aproksymator

--- aproksymator_czebyszew.c
+++ aproksymator_czebyszew.c
@@ -4,10 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
+#include <math.h>
 
 // I - [-1, 1]
-// G(x) = 1/(1-x^2)^(0.5)
-// Tn(x) = cos(n * arccos(x)), as n = 0,1,2,..,oo
+// w(x) = 1/(1-x^2)^(0.5) waga
+// Tn(x) = cos(n * arccos(x)), as n = 0,1,2,..,oo 
 // rekurencyjny zapis Tn(x) = 2x * Tn-1(x) - Tn-2(x) = 0.5 * ((x + (x^2 - 1)^0.5)^k + (x - (x^2 - 1)^0.5)^k), gdzie
 // x = cos(t), k = 0,1,2,...
 
@@ -18,16 +19,18 @@
 		i - numer funkcji 
 		x - wspolrzedna dla ktorej obliczana jest wartosc funkcji
  */
-double fi(double a, double b, int n, int i, double x)
+double fi(int ilosc_funkcji, int stopien){}
 
 /* Pierwsza pochodna fi */
-double dfi(double a, double b, int n, int i, double x)
+double dfi(int ilosc_funkcji, int stopien){}
 
 /* Druga pochodna fi */
-double d2fi(double a, double b, int n, int i, double x)
+double d2fi(int ilosc_funkcji, int stopien){}
 
 /* Trzecia pochodna fi */
-double d3fi(double a, double b, int n, int i, double x)
+double d3fi(int ilosc_funkcji, int stopien){}
 
 /* Pomocnicza f. do rysowania bazy */
-double xfi(double a, double b, int n, int i, FILE *out)
+double xfi(int ilosc_funkcji, int stopien, FILE *out){
+
+}
